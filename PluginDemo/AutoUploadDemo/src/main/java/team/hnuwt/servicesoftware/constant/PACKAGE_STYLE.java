218b2294cbd775fecc7d41b2bc19b681e8e7122c
cyclone1517 <cyl1517@yeah.net>
2019-02-21 20:47:43 +0800

    MODIFY: 需要让一个插件适配多种报文类型

---
+++ PluginDemo/AutoUploadDemo/src/main/java/team/hnuwt/servicesoftware/constant/PACKAGE_STYLE.java
@@ -0,0 +1,12 @@
+package team.hnuwt.servicesoftware.constant;
+
+public class PACKAGE_STYLE {
+
+    private static final String AUTO_UPLOAD[] = new String[]{"firstStartChar", "firstLength", "secondLength",
+            "secondStartChar", "control", "address", "afn", "seq", "dataId", "number", "id", "data", "state", "cs",
+            "endChar"};
+
+    private static final String HEARTBEAT[] = new String[]{"firstStartChar", "firstLength", "secondLength",
+            "secondStartChar", "control", "address", "afn", "seq", "dataId", "sigIntnsBCD", "cs",
+            "endChar"};
+}
