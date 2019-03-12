36eaaf581b43f39d7556d14ac1baf9a8e36ee566
cyclone1517 <cyl1517@yeah.net>
2019-03-12 16:39:44 +0800

    ADD: add method 'bytes2hex' to print bytes set

--- ServiceServerModule/src/main/java/team/hnuwt/servicesoftware/server/util/PkgPackUtil.java
+++ ServiceServerModule/src/main/java/team/hnuwt/servicesoftware/server/util/PkgPackUtil.java
@@ -83,4 +83,23 @@ public class PkgPackUtil {
         return "";
     }
 
+    /**
+     * 为了回显数据，没有实质功能的作用
+     * @param bytes
+     * @return
+     */
+    public static String bytes2hex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        String tmp = null;
+        for (byte b : bytes) {
+            // 将每个字节与0xFF进行与运算，然后转化为10进制，然后借助于Integer再转化为16进制
+            tmp = Integer.toHexString(0xFF & b);
+            if (tmp.length() == 1) {
+                tmp = "0" + tmp;
+            }
+            sb.append(tmp);
+        }
+        return sb.toString();
+
+    }
 }
