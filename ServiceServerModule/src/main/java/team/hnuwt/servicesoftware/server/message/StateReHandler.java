36b1498426285c22ec7365703a32a537111f09d6
cyclone1517 <cyl1517@yeah.net>
2019-03-13 16:46:26 +0800

    ADD: new handler to reply operation state

---
+++ ServiceServerModule/src/main/java/team/hnuwt/servicesoftware/server/message/StateReHandler.java
@@ -0,0 +1,47 @@
+package team.hnuwt.servicesoftware.server.message;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import team.hnuwt.servicesoftware.server.constant.up.CONSTANT;
+import team.hnuwt.servicesoftware.server.util.ByteBuilder;
+import team.hnuwt.servicesoftware.server.util.FieldPacker;
+import team.hnuwt.servicesoftware.server.util.ProduceUtil;
+
+import java.nio.channels.SocketChannel;
+
+/**
+ * 抄表回复处理类
+ */
+public class StateReHandler implements Runnable{
+
+    private ByteBuilder meterData;
+    private SocketChannel sc;
+    private boolean success;
+    private Logger logger = LoggerFactory.getLogger(StateReHandler.class);
+    private static final int[] fieldlen = {4, 8};       /* 数据域单元每个域的长度，表序号4位，表读数8位，阀门状态2位 */
+
+    public StateReHandler(SocketChannel sc, ByteBuilder meterData, boolean success){
+        this.sc = sc;
+        this.meterData = meterData;
+        this.success = success;
+    }
+
+    @Override
+    public void run() {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode root = mapper.createObjectNode();
+        String result = meterData.toString();
+
+        String addr = result.substring(CONSTANT.ADD_START, CONSTANT.ADD_END);
+
+
+        root.put("addr", addr);
+        root.put("success", success);
+
+        ProduceUtil.addQueue("UPSTREAM", "CTRL_ONOFF", root.toString());
+
+    }
+}
