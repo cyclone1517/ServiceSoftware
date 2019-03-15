f66776fcf565ba53e775553ef55720d4ee510d4f
cyclone1517 <cyl1517@yeah.net>
2019-03-15 18:23:54 +0800

    MODIFY: changed package location.

--- ServicePluginModule/src/main/java/team/hnuwt/servicesoftware/plugin/service/PluginService.java
+++
@@ -1,7 +0,0 @@
-package team.hnuwt.servicesoftware.plugin.service;
-
-import team.hnuwt.servicesoftware.plugin.util.ByteBuilder;
-
-public interface PluginService {
-    public void translate(ByteBuilder pkg);
-}
