fbb059d00c5204c0c7d1d881f32a00c6fe907639
cyclone1517 <cyl1517@yeah.net>
2019-03-18 00:38:10 +0800

    MODIFY: database construction sql code

--- servicesoftware.sql
+++ servicesoftware.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50717
+Source Server         : localdb
+Source Server Version : 80012
 Source Host           : localhost:3306
 Source Database       : servicesoftware
 
 Target Server Type    : MYSQL
-Target Server Version : 50717
+Target Server Version : 80012
 File Encoding         : 65001
 
-Date: 2018-12-14 21:03:52
+Date: 2019-03-17 23:31:07
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,5 +23,29 @@ CREATE TABLE `tb_data` (
   `collectorId` int(11) NOT NULL,
   `meterId` int(11) NOT NULL,
   `data` int(11) NOT NULL,
-  `state` int(11) NOT NULL
+  `state` int(11) NOT NULL,
+  PRIMARY KEY (`collectorId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_heartbeat
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_heartbeat`;
+CREATE TABLE `tb_heartbeat` (
+  `collectorId` int(11) NOT NULL,
+  `port` int(11) NOT NULL,
+  `time` timestamp NOT NULL,
+  PRIMARY KEY (`collectorId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_login
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_login`;
+CREATE TABLE `tb_login` (
+  `collectorId` int(11) NOT NULL,
+  `port` int(11) NOT NULL,
+  `state` tinyint(4) NOT NULL,
+  `time` timestamp NOT NULL,
+  PRIMARY KEY (`collectorId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
