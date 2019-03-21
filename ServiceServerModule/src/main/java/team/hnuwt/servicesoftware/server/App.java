585f161b526a6e41d921829e15393bdcf4ffcfba
cyclone1517 <cyl1517@yeah.net>
2019-03-21 17:50:11 +0800

    MODIFT: comments

--- ServiceServerModule/src/main/java/team/hnuwt/servicesoftware/server/App.java
+++ ServiceServerModule/src/main/java/team/hnuwt/servicesoftware/server/App.java
@@ -1,7 +1,10 @@
 package team.hnuwt.servicesoftware.server;
 
 import java.io.IOException;
+import java.util.Timer;
 
+import team.hnuwt.servicesoftware.server.compatible.CollecSimulator;
+import team.hnuwt.servicesoftware.server.compatible.HeartBeatTask;
 import team.hnuwt.servicesoftware.server.service.MainReactor;
 import team.hnuwt.servicesoftware.server.service.UDPReactor;
 import team.hnuwt.servicesoftware.server.util.ConsumerUtil;
@@ -10,9 +13,11 @@ public class App {
 
     public static void main(String[] args) throws IOException
     {
+        // 启动集中器连接和数据读取侦听
         new Thread(new MainReactor()).start();
         new Thread(new UDPReactor()).start();
 
+        // 启动三方和内部消息命令侦听
         ConsumerUtil consumerUtil = new ConsumerUtil();
         consumerUtil.run();
     }
