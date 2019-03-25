e7ae65e9b5e1d121fdcdfc1f14d73462ffd90f2b
cyclone1517 <cyl1517@yeah.net>
2019-03-25 17:48:18 +0800

    MODIFY: get instance previously to initialize the class.

--- ServiceProtocolModule/src/main/java/team/hnuwt/servicesoftware/protocol/App.java
+++ ServiceProtocolModule/src/main/java/team/hnuwt/servicesoftware/protocol/App.java
@@ -6,8 +6,11 @@ import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import team.hnuwt.servicesoftware.plugin.service.PluginService;
+import team.hnuwt.servicesoftware.plugin.util.ByteBuilder;
 import team.hnuwt.servicesoftware.protocol.util.ConsumerUtil;
 import team.hnuwt.servicesoftware.protocol.util.FileUtil;
+import team.hnuwt.servicesoftware.protocol.util.PluginUtil;
 
 public class App {
 
@@ -23,6 +26,14 @@ public class App {
             props.load(App.class.getClassLoader().getResourceAsStream(APPLICATION_FILE));
         } catch (IOException e) {
             logger.error("", e);
+        }
+
+        try {
+            PluginService ps = PluginUtil.getInstance(Long.valueOf("30081548684"));
+//            ps.translate(new ByteBuilder("683500350068C9000000000002701000040012BC16"));
+            Thread.sleep(500);  /* 获得类实例，提前做类静态化 */
+        } catch (Exception e){
+            logger.info("Initiating");
         }
 
         ConsumerUtil consumerUtil = new ConsumerUtil();
