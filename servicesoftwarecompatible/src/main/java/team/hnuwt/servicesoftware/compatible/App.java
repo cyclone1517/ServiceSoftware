398b30f63f8c91efcc0204ef26cdcf1f3477e7db
cyclone1517 <cyl1517@yeah.net>
2019-03-28 14:12:03 +0800

    ADD: if device cut a link, the frontend would notify compatible mode by subscribed redisUtil.

--- servicesoftwarecompatible/src/main/java/team/hnuwt/servicesoftware/compatible/App.java
+++ servicesoftwarecompatible/src/main/java/team/hnuwt/servicesoftware/compatible/App.java
@@ -1,7 +1,9 @@
 package team.hnuwt.servicesoftware.compatible;
 
+import team.hnuwt.servicesoftware.compatible.server.DataSubscribeService;
 import team.hnuwt.servicesoftware.compatible.server.MainReactor;
 import team.hnuwt.servicesoftware.compatible.util.CompatibleUtil;
+import team.hnuwt.servicesoftware.compatible.util.Subscriber;
 
 import java.io.IOException;
 
@@ -14,6 +16,12 @@ public class App {
 
         // 启动集中器连接和数据读取侦听
         new Thread(mainReactor).start();
+
+        // 启动 redis 数据订阅服务
+        Subscriber subscriber = new Subscriber();
+        String myChanel = "agency";
+        DataSubscribeService dss = new DataSubscribeService(subscriber, myChanel);
+        dss.run();
     }
 
 }
