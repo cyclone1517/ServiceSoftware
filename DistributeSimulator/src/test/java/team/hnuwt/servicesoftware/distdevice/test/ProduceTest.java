7e15f6e7ac8345fe9c41563a2c307681b07ec4a3
cyclone1517 <cyl1517@yeah.net>
2019-04-01 16:12:22 +0800

    ADD: property file and logback

--- DistributeSimulator/src/test/java/team/hnuwt/servicesoftware/distdevice/test/ProduceTest.java
+++ DistributeSimulator/src/test/java/team/hnuwt/servicesoftware/distdevice/test/ProduceTest.java
@@ -6,12 +6,39 @@ import team.hnuwt.servicesoftware.disdevice.mode.Archive;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author yuanlong chen
  * 来自中间服务的消息示例
  */
 public class ProduceTest {
+
+    @Test
+    public void choseOrder(){
+        Scanner s = new Scanner(System.in);
+        boolean out = false;
+        while (true){
+            System.out.println("please enter a choice...\n");
+            int choice = s.nextInt();
+            switch (choice){
+                case -1:
+                    out = true;
+                    break;
+                case 1:
+                    sendReadMeter();
+                    break;
+                case 2:
+                    sendOn();
+                    break;
+                case 3:
+                    sendArchive();
+                    break;
+            }
+
+            if (out) break;
+        }
+    }
 
     @Test
     public void sendReadMeter() {
@@ -44,23 +71,24 @@ public class ProduceTest {
 
         List<Archive> archives = new ArrayList<>();
         Archive ac1 = new Archive();
-        ac1.setId("0100");
+        ac1.setId("0800");
         ac1.setMadd("080104151101");
         ac1.setPrtc("01");
         ac1.setCadd("00000000");
         ac1.setPort("04");
         archives.add(ac1);
 
-        Archive ac2 = new Archive();
-        ac2.setId("0100");
-        ac2.setMadd("080104151101");
-        ac2.setPrtc("01");
-        ac2.setCadd("00000000");
-        ac2.setPort("04");
-        archives.add(ac2);
+//        Archive ac2 = new Archive();
+//        ac2.setId("0100");
+//        ac2.setMadd("080104151101");
+//        ac2.setPrtc("01");
+//        ac2.setCadd("00000000");
+//        ac2.setPort("04");
+//        archives.add(ac2);
 
-        String msg = ProduceTestUtil.geneArchive("1021", archives.size(), archives);
+        String msg = ProduceTestUtil.geneArchive("1020", archives.size(), archives);
         ProduceUtil.addQueue(topic, tag, msg);
+
     }
 
     @Test
@@ -117,6 +145,14 @@ public class ProduceTest {
         String msg = "685500550068880000FD03008C60100001070100010000000015FFA216";
         ProduceUtil.addQueue(topic, tag, msg);
 
+    }
+
+    private void waitForSending(){
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
