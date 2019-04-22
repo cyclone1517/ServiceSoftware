b605f3c0a1119a09faf9f46b1d7170be1f1e7e3f
cyclone1517 <cyl1517@yeah.net>
2019-04-22 16:28:07 +0800

    ADD: supplementary heartbeat for each response of device.

---
+++ ServiceServerModule/src/main/java/team/hnuwt/servicesoftware/server/model/HeartData.java
@@ -0,0 +1,24 @@
+package team.hnuwt.servicesoftware.server.model;
+
+import java.sql.Timestamp;
+
+public class HeartData {
+    private long addr;
+    private Timestamp timestamp;
+
+    public long getAddr() {
+        return addr;
+    }
+
+    public void setAddr(long addr) {
+        this.addr = addr;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
