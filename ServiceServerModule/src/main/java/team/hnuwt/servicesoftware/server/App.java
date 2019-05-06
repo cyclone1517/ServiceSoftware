5518559972f762e0c991f71cc9a8d09c7bc38782
cyclone1517 <cyl1517@yeah.net>
2019-05-06 14:11:35 +0800

    ADD: comments

--- ServiceServerModule/src/main/java/team/hnuwt/servicesoftware/server/App.java
+++ ServiceServerModule/src/main/java/team/hnuwt/servicesoftware/server/App.java
@@ -12,16 +12,16 @@ public class App {
 
     public static void main(String[] args) throws IOException
     {
-        MainReactor mainReactor = new MainReactor();
-        CompatibleUtil.setMainReactor(mainReactor);
-        TCPMessageHandler.openTCPCompatible();
+        MainReactor mainReactor = new MainReactor();    /* 启动主反应器：管理连接 */
+        CompatibleUtil.setMainReactor(mainReactor);     /* 注册与兼容模块的通信连接 */
+        TCPMessageHandler.openTCPCompatible();          /* 打开本地兼容模式开关 */
 
         // 启动集中器连接和数据读取侦听
-        new Thread(mainReactor).start();
+        new Thread(mainReactor).start();                /* 启动主反应器 */
 //        new Thread(new UDPReactor()).start();
 
         // 启动三方和内部消息命令侦听
-        ConsumerUtil consumerUtil = new ConsumerUtil();
+        ConsumerUtil consumerUtil = new ConsumerUtil(); /* 消息队列：负责中间服务命令接受 */
         consumerUtil.run();
     }
 
