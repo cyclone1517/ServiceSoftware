c9a8cd1f812ad14c486f0674ef66e44f4e448f31
cyclone1517 <cyl1517@yeah.net>
2019-05-10 18:12:50 +0800

    MODIFY: move login and heartbeat from Mysql to Redis

--- database.sql
+++ database.sql
@@ -54,17 +54,6 @@ CREATE TABLE `tb_duplicate`  (
 ) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for tb_login
--- ----------------------------
-DROP TABLE IF EXISTS `tb_login`;
-CREATE TABLE `tb_login`  (
-  `collectorId` int(11) NOT NULL,
-  `state` tinyint(4) NOT NULL,
-  `time` timestamp(0) NOT NULL,
-  PRIMARY KEY (`collectorId`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
 -- View structure for maxid
 -- ----------------------------
 DROP VIEW IF EXISTS `maxid`;
